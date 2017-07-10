481827ee0ce6d76bbfbfcc5b5b5d3ba7c138f3f6
Sergey Voronovich <serj.voronovich@gmail.com>
2017-07-10 14:04:21 +0300

    - optimized mapWebRoutes()

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -52,13 +52,9 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapWebRoutes()
     {
-        return request()->wantsJson()
-            ? Route::middleware('api')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/web.php'))
-            : Route::middleware('web')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/web.php'));
+        return Route::middleware(request()->wantsJson() ? 'api' : 'web')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/web.php'));
     }
 
     /**
