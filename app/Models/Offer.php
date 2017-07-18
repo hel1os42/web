c4ada90c724f9680f95b92be0b0e4954885357d3
lex <lex.codeworld@gmail.com>
2017-07-18 21:07:29 +0300

    Offer model. Without relation.

---
+++ app/Models/Offer.php
@@ -0,0 +1,226 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+use MichaelAChrisco\ReadOnly\ReadOnlyTrait;
+
+/**
+ * Class Offer
+ * @package App
+ *
+ * @property string id
+ * @property int acc_id
+ * @property string name
+ * @property string descr
+ * @property float reward
+ * @property string status
+ * @property Carbon dt_start
+ * @property Carbon dt_finish
+ * @property Carbon tm_start
+ * @property Carbon tm_finish
+ * @property string country
+ * @property string city
+ * @property string categ
+ * @property int max_count
+ * @property int max_for_user
+ * @property int max_per_day
+ * @property int max_for_user_per_day
+ * @property int min_level
+ * @property double lat
+ * @property double lng
+ * @property int radius
+ * @property Carbon created_at
+ * @property Carbon updated_at
+ */
+class Offer extends Model
+{
+    use ReadOnlyTrait;
+
+    /** @var string */
+    private $table = "offer";
+
+    /** @var array */
+    private $timestamps = ['created_at', 'updated_at'];
+
+    /** @var string */
+    private $primaryKey = 'id';
+
+    /** @var array */
+    protected $fillable = [
+        'acc_id',
+        'name',
+        'descr',
+        'reward',
+        'status',
+        'dt_start',
+        'dt_finish',
+        'tm_start',
+        'tm_finish',
+        'country',
+        'city',
+        'categ',
+        'max_count',
+        'max_for_user',
+        'max_per_day',
+        'max_for_user_per_day',
+        'min_level',
+        'lat',
+        'lng',
+        'radius',
+    ];
+
+    /** @var array */
+    protected $casts = [
+        'id'                    => 'string',
+        'acc_id'                => 'int',
+        'name'                  => 'string',
+        'descr'                 => 'string',
+        'reward'                => 'float',
+        'status'                => 'string',
+        'dt_start'              => 'date',
+        'dt_finish'             => 'date',
+        'tm_start'              => 'datetime',
+        'tm_finish'             => 'datetime',
+        'country'               => 'string',
+        'city'                  => 'string',
+        'categ'                 => 'string',
+        'max_count'             => 'int',
+        'max_for_user'          => 'int',
+        'max_per_day'           => 'int',
+        'max_for_user_per_day'  => 'int',
+        'min_level'             => 'int',
+        'lat'                   => 'double',
+        'lng'                   => 'double',
+        'radius'                => 'int',
+    ];
+
+    /** @return string */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /** @return int */
+    public function getAccId(): int
+    {
+        return $this->acc_id;
+    }
+
+    /** @return string */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /** @return string */
+    public function getDescr(): string
+    {
+        return $this->descr;
+    }
+
+    /** @return float */
+    public function getReward(): float
+    {
+        return $this->reward;
+    }
+
+    /** @return string */
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    /** @return Carbon */
+    public function getDtStart(): Carbon
+    {
+        return Carbon::parse($this->dt_start);
+    }
+
+    /** @return Carbon */
+    public function getDtFinish(): Carbon
+    {
+        return Carbon::parse($this->dt_finish);
+    }
+
+    /** @return Carbon */
+    public function getTmStart(): Carbon
+    {
+        return Carbon::parse($this->tm_start);
+    }
+
+    /** @return Carbon */
+    public function getTmFinish(): Carbon
+    {
+        return Carbon::parse($this->tm_finish);
+    }
+
+    /** @return string */
+    public function getCountry(): string
+    {
+        return $this->country;
+    }
+
+    /** @return string */
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+
+    /** @return string */
+    public function getCateg(): string
+    {
+        return $this->categ;
+    }
+
+    /** @return int */
+    public function getMaxCount(): int
+    {
+        return $this->max_count;
+    }
+
+    /** @return int */
+    public function getMaxForUser(): int
+    {
+        return $this->max_for_user;
+    }
+
+    /** @return int */
+    public function getMaxPerDay(): int
+    {
+        return $this->max_per_day;
+    }
+
+    /** @return int */
+    public function getMaxForUserPerDay(): int
+    {
+        return $this->max_for_user_per_day;
+    }
+
+    /** @return int */
+    public function getMinLevel(): int
+    {
+        return $this->min_level;
+    }
+
+    /** @return double */
+    public function getLat(): double
+    {
+        return $this->lat;
+    }
+
+    /** @return double */
+    public function getLng(): double
+    {
+        return $this->lng;
+    }
+
+    /** @return int */
+    public function getRadius(): int
+    {
+        return $this->radius;
+    }
+
+    // TODO: relation with account
+}
