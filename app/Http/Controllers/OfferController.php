3528b35df0b2d9b4316108c48de78f11d0a0f7fc
Aleksandr Stoilovskyi <mobixon@gmail.com>
2017-07-19 18:45:02 +0300

    create and store methods? in process...

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 
 class OfferController extends Controller
@@ -10,7 +11,7 @@ class OfferController extends Controller
     /**
      * Display offer listing.
      *
-     * @return Response|\Illuminate\Http\JsonResponse
+     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
      */
     public function index()
     {
@@ -20,29 +21,67 @@ class OfferController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return Response|\Illuminate\Http\JsonResponse
+     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
      */
     public function create()
     {
-        response()->render('');
+        response()->render('offer.create', [
+            'name' => null,
+            'description' => null,
+            'reward' => 1,
+            'dateStart' => Carbon::createFromDate(),
+            'timeStart' => Carbon::createFromTime(),
+            'datefinish' => null,
+            'timefinish' => null,
+            'category' => 'Test', //todo @mobixon create category table+model
+            'max_count' => 10,
+            'max_for_user' => 1,
+            'max_per_day' => 10,
+            'max_for_user_per_day' => 1,
+            'min_level' => 1,
+            'latitude' => null,
+            'longitude' => null,
+            'radius' => null,
+            'country' => null,
+            'city' => null
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return Response|\Illuminate\Http\JsonResponse
+     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
-
+        response()->render('offer.show', [
+//            'name' => null,
+//            'description' => null,
+//            'reward' => 1,
+//            'date_start' => Carbon::createFromDate(),
+//            'time_start' => Carbon::createFromTime(),
+//            'date_finish' => null,
+//            'time_finish' => null,
+//            'category' => 'Test',
+//            'max_count' => 10,
+//            'max_for_user' => 1,
+//            'max_per_day' => 10,
+//            'max_for_user_per_day' => 1,
+//            'min_level' => 1,
+//            'latitude' => null,
+//            'longitude' => null,
+//            'radius' => null,
+//            'country' => null,
+//            'city' => null
+        ]);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  string  $id
-     * @return Response|\Illuminate\Http\JsonResponse
+     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
      */
     public function show(string $id)
     {
@@ -53,7 +92,7 @@ class OfferController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param  string  $id
-     * @return Response|\Illuminate\Http\JsonResponse
+     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
      */
     public function edit(string $id)
     {
@@ -65,7 +104,7 @@ class OfferController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  string  $id
-     * @return Response|\Illuminate\Http\JsonResponse
+     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
      */
     public function update(Request $request,string $id)
     {
