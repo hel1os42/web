81902be9169d2946d8d017fd38ab4032ca4cf2a0
Aleksandr Stoilovskyi <mobixon@gmail.com>
2017-07-20 13:23:17 +0300

    fix warnings founded by phpcs

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -25,19 +25,6 @@ class Handler extends ExceptionHandler
     ];
 
     /**
-     * Report or log an exception.
-     *
-     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-     *
-     * @param  \Exception  $exception
-     * @return void
-     */
-    //    public function report(Exception $exception)
-    //    {
-    //        parent::report($exception);
-    //    }
-
-    /**
      * Render an exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request  $request
