54b93deef2a55f9ffd77e6016b5181c925ba76cf
Aleksandr Stoilovskyi <mobixon@gmail.com>
2017-08-10 11:07:40 +0300

    add qrcode to view

--- resources/views/redemption/code.blade.php
+++ resources/views/redemption/code.blade.php
@@ -1,1 +1,6 @@
+<script type="text/javascript" src="/js/qrcode.min.js"></script>
 <h1>{{$activation_code}}</h1>
+<p id="qrcode"></p>
+<script type="text/javascript">
+    new QRCode(document.getElementById("qrcode"), "{{$activation_code}}");
+</script>
