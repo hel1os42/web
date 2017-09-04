3990ae2783555e44b3b60baf98be2f105a5f983c
lex <lex.codeworld@gmail.com>
2017-09-04 14:00:19 +0300

    fixed in validator some field name

--- app/Http/Requests/TransactRequest.php
+++ app/Http/Requests/TransactRequest.php
@@ -34,7 +34,7 @@ class TransactRequest extends FormRequest
     {
         return [
             'source'      => 'required|ownAddress',
-            'destination' => 'required|different:sender|exists:pgsql_nau.account,addr',
+            'destination' => 'required|different:source|exists:pgsql_nau.account,addr',
             'amount'      => 'required|numeric|min:0.0001',
         ];
     }
