0c2176a7bd4a9f66102310b7b17b6f0e0d168235
Aleksandr Stoilovskyi <mobixon@gmail.com>
2017-09-12 10:01:38 +0300

    fixes

--- app/Models/AdditionalField.php
+++ app/Models/AdditionalField.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models\AdditionalField;
+namespace App\Models;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Builder;
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Relation::morphMap([
-            'users' => 'App\Models\User'
+            'users' => \App\Models\User::class
         ]);
     }
 
