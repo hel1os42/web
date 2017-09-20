2003af7469a04b25b431fb1c0d9c70296a480e04
Necromancer <necromancer@toavalon.com>
2017-09-20 21:41:46 +0300

    part of categories seeder

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,7 +2,6 @@
 
 namespace App\Console;
 
-use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
@@ -13,7 +12,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        //Test::class
     ];
 
     /**
--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -1,10 +1,33 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Webpatser\Uuid\Uuid;
 
 class CategoriesTableSeeder extends Seeder
 {
+    private $categories = [
+        'Food & Drinks' => [
+            'Food'   => [
+                'Healthy food',
+                'Meal food',
+                'Vegetarian cuisine',
+                'Italian cuisine',
+                'Indian cuisine',
+                'Mexican cuisine',
+                'BBQ Grill',
+            ],
+            'Drinks' => [
+                'Alcoholic',
+                'Non alcoholic',
+            ]
+        ],
+        'Beauty & Fitness',
+        'Retail & Services',
+        'Attractions & Leisure',
+        'Other & Online'
+    ];
+
     /**
      * Run the database seeds.
      *
@@ -12,13 +35,43 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        for ($i = 1; $i <= 3; $i++) {
-            \Illuminate\Support\Facades\DB::table('categories')->insert([
-                'id'         => Uuid::generate(),
-                'name'       => 'Category ' . (string)$i,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ]);
+        $categories = $this->getCategories();
+        foreach ($categories as $category) {
+            \Illuminate\Support\Facades\DB::table('categories')->insert($category);
+        }
+    }
+
+    private function getCategories(array $categories = null, string $parent = null): iterable
+    {
+        if (null === $categories) {
+            $categories = $this->categories;
+        }
+
+        $lastCategoryId = null;
+        foreach ($categories as $category => $children) {
+            $lastCategoryId = Uuid::generate(4)->__toString();
+
+            $categoryArray = [
+                'id'         => $lastCategoryId,
+                'name'       => $category,
+                'created_at' => Carbon::now()->__toString(),
+                'updated_at' => Carbon::now()->__toString()
+            ];
+
+            if (null !== $parent) {
+                $categoryArray['parent_id'] = $parent;
+            }
+
+            if (is_array($children)) {
+                yield $categoryArray;
+
+                foreach ($this->getCategories($children, $lastCategoryId) as $item) {
+                    yield $item;
+                }
+                continue;
+            }
+
+            yield array_merge($categoryArray, ['name' => $children]);
         }
     }
 }
