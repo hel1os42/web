3094cfee6486c11aa0a29f2cea76188206c4a6fd
skido <doskich@toavalon.com>
2017-09-25 15:58:09 +0300

    #44: added categories filter for offers; offer scopes moved into trait.

---
+++ app/Helpers/Constants.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: skido
+ * Date: 25.09.17
+ * Time: 15:10
+ */
+namespace App\Helpers;
+
+/**
+ * Class Constants
+ */
+class Constants
+{
+    /**
+     * uuid regular expression
+     */
+    const UUID_REGEX = '/^[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}$/';
+}
