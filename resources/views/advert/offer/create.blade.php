895ecfcf40a7a4dae60c6f95089ce8f7d44f61be
iLyK Necromancer <necromancer@toavalon.com>
2017-09-26 11:23:46 +0300

    [develop][hotfix] allow SessionGuard to authenticate when request wants JSON.

--- resources/views/advert/offer/create.blade.php
+++ resources/views/advert/offer/create.blade.php
@@ -52,6 +52,7 @@
         };
 
         xmlhttp.open("GET", "{{route('categories')}}", true);
+        xmlhttp.setRequestHeader('Accept', 'application/json');
         xmlhttp.send();
     </script>
 
