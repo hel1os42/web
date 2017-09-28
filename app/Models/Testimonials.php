3cf08c674ea78c763b4324e96b210aa5d25237c7
Aleksandr Stoilovskyi <mobixon@gmail.com>
2017-09-28 10:50:48 +0300

    create model and migration for testimonials

---
+++ app/Models/Testimonials.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\NauModels\Offer;
+use App\Traits\Uuids;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+/**
+ * Class Testimonials
+ * @package App\Models
+ *
+ * @property string id
+ * @property string text
+ * @property string user_id
+ * @property string offer_id
+ * @property integer stars
+ * @property Carbon created_at
+ * @property Carbon updated_at
+ *
+ */
+class Testimonials extends Model
+{
+    use Uuids;
+
+    /**
+     * Testimonials constructor.
+     *
+     * @param array $attributes
+     */
+    public function __construct(array $attributes = [])
+    {
+        $this->connection = config('database.default');
+
+        $this->table      = 'testimonials';
+        $this->primaryKey = 'id';
+
+        $this->initUuid();
+
+        $this->casts = [
+            'id'       => 'string',
+            'user_id'  => 'string',
+            'offer_id' => 'string',
+            'text'     => 'string',
+            'stars'    => 'integer'
+        ];
+
+        $this->hidden = [
+            'created_at',
+            'updated_at',
+        ];
+
+        parent::__construct($attributes);
+    }
+
+    /** @return string */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /** @return string */
+    public function getText(): string
+    {
+        return $this->text;
+    }
+
+    /** @return string */
+    public function getStars(): string
+    {
+        return $this->stars;
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function offer(): BelongsTo
+    {
+        return $this->belongsTo(Offer::class);
+    }
+}
---
+++ database/migrations/2017_09_28_070317_create_testimonials_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTestimonialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('testimonials', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('user_id')->index();
+            $table->uuid('offer_id')->index();
+            $table->text('text');
+            $table->integer('stars');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('testimonials');
+    }
+}
