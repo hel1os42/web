14f991efd1fe12c5630a813a396f0233185d6890
Aleksandr Stoilovskyi <mobixon@gmail.com>
2017-10-09 17:42:46 +0300

    #65 edit admin creation seeder

--- database/seeds/SetAdminRoleSeeder.php
+++ database/seeds/SetAdminRoleSeeder.php
@@ -12,11 +12,24 @@ class SetAdminRoleSeeder extends Seeder
      */
     public function run()
     {
-        $user = User::query()->where('email', 'test@test.com')->first();
+        $user = new User([
+            'name'     => 'Admin',
+            'email'    => 'sadm@nau.io',
+            'password' => 'jf747hsf',
+            'phone' => '+380123456789'
+        ]);
 
-        if ($user instanceof User) {
-            if (class_exists('\App\Models\Role')) {
-                $user->roles()->attach(\App\Models\Role::findByName('admin'));
+        try {
+            $user->save();
+
+            if ($user instanceof User) {
+                if (class_exists('\App\Models\Role')) {
+                    $user->roles()->attach(\App\Models\Role::findByName('admin')->getId(), \App\Models\Role::findByName('user')->getId());
+                }
+            }
+        } catch (\Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException $exception) {
+            if (app('env') !== 'testing') {
+                throw $exception; // ignore exception for testing environment
             }
         }
     }
