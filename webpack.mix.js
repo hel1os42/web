054604549d8c3b3d062a2532fd2edfdbab0c612e
pasha <avdgian.pavel@gmail.com>
2017-10-12 19:29:52 +0300

    initial

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,5 @@ const { mix } = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
-
-
+   .sass('resources/assets/sass/app.scss');
+   .copyDirectory('resources/assets/css/', 'public/css2');
