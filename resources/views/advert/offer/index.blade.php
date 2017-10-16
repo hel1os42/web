d3e67fdc26dd3b4afedc5ecf58e3ac8c9aa6e19d
pasha <avdgian.pavel@gmail.com>
2017-10-16 13:36:46 +0300

    img & clickable tr

--- resources/views/advert/offer/index.blade.php
+++ resources/views/advert/offer/index.blade.php
@@ -3,17 +3,27 @@
 @section('title', 'List offers')
 
 @section('content')
-
+@php
+    foreach ($data as &$offer) {
+        $offer = array_merge(['picture_url' => $offer['picture_url']], $offer);
+    }
+@endphp
 <div class="col-md-12">
     <h3 class="title">Offers</h3>
     <div class="card card-very-long">
         <div class="content">
             <div class="table-responsive card-very-long-children">
-                <table class="table">
+                <table class="table table-hover">
                     <thead class="text-primary">
 						<tr>
                             @foreach (array_keys($data[0]) as $offerField)
-							    <th> {{ $offerField }} </th>
+                                    @if ($offerField != 'id')
+                                        @if ($offerField == 'picture_url')
+    			    				        <th> picture </th>
+                                        @else
+    			    				        <th> {{ $offerField }} </th>
+                                        @endif
+                                    @endif
                             @endforeach
 						</tr>
                         
@@ -21,18 +31,20 @@
                     <tbody>
 
                         @foreach ($data as $offer)
-                        <tr>
+                        <tr class="clickable-table-row" data-uuid="{{route('advert.offers.show', $offer['id'])}}">
                             @foreach($offer as $key => $row)
                                 @if (empty($row))
                                     <td> - </td>
                                 @else
+                                    @if ($key != 'id')
                                     <td> 
-                                        @if ($key == 'id')
-                                            <a href="{{route('advert.offers.show', $row)}}">{{ $row }} </a>
-                                        @else
-                                            {{ $row }} 
-                                        @endif
+                                            @if ($key === 'picture_url')
+                                                    <img id="img-{{$offer['id']}}" src="{{ $row }}" alt="...">
+                                            @else
+                                                {{ $row }} 
+                                            @endif
                                     </td>
+                                    @endif
                                 @endif
                             @endforeach
                             <td> @include('partials/offer-picture-filepicker', ['offerId' => $offer['id']]) </td>
--- resources/views/user/profile/referrals.blade.php
+++ resources/views/user/profile/referrals.blade.php
@@ -1,18 +1,28 @@
 @extends('layouts.master')
 
 @section('title', 'Profile')
-
+@php
+    foreach ($data as &$offer) {
+        $offer = array_merge(['picture_url' => $offer['picture_url']], $offer);
+    }
+@endphp
 @section('content')
     <div class="col-md-12">
         <h3>Referrals</h3>
         <div class="card card-very-long">
             <div class="content">
                 <div class="table-responsive card-very-long-children">
-                    <table class="table">
+                    <table class="table table-hover">
                         <thead class="text-primary">
 			    			<tr>
                                 @foreach (array_keys($data[0]) as $referralField)
-			    				    <th> {{ $referralField }} </th>
+                                    @if ($referralField != 'id')
+                                        @if ($referralField == 'picture_url')
+    			    				        <th> picture </th>
+                                        @else
+    			    				        <th> {{ $referralField }} </th>
+                                        @endif
+                                    @endif
                                 @endforeach
 			    			</tr>
                             
@@ -20,12 +30,20 @@
                         <tbody>
 
                             @foreach ($data as $referral)
-                            <tr>
+                            <tr class="clickable-table-row" data-uuid="{{route('users.show', $referral['id'])}}">
                                 @foreach($referral as $key => $row)
                                     @if (empty($row))
                                         <td> - </td>
                                     @else
-                                        <td> {{ $row }} </td>
+                                        @if ($key != 'id')
+                                            @if ($key === 'picture_url')
+                                                <td> 
+                                                    <img src="{{ $row }}" alt="">
+                                                </td>
+                                            @else
+                                                <td> {{ $row }} </td>
+                                            @endif
+                                        @endif
                                     @endif
                                 @endforeach
                             </tr>
