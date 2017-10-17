001db36ecbfc492a71f2410e2b26dc113f4638cf
pasha <avdgian.pavel@gmail.com>
2017-10-17 08:54:20 +0300

    changes

--- build_front.sh
+++ build_front.sh
@@ -5,7 +5,6 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | b
     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && \ 
     npm config delete prefix && \
     nvm install 6.9.1 && \
-    nvm use --delete-prefix v6.9.1 && \
     npm install && \
     npm install -g gulp && \
     npm link gulp && \
