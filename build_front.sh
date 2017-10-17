eb107396ba48966bc0078753d593e17a3ef2b112
pasha <avdgian.pavel@gmail.com>
2017-10-17 09:04:41 +0300

    frustration4

--- build_front.sh
+++ build_front.sh
@@ -2,7 +2,7 @@
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash && \
     export NVM_DIR="/root/.nvm" && \
-    bash "$NVM_DIR/nvm.sh" && \ 
+    source "$NVM_DIR/nvm.sh" && \ 
     npm config delete prefix && \
     nvm install 6.9.1 && \
     npm install && \
