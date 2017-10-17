23f96f23e631bc7ee756677a992e7dbf42bc9986
pasha <avdgian.pavel@gmail.com>
2017-10-17 09:43:48 +0300

    frustration9

--- build_front.sh
+++ build_front.sh
@@ -3,9 +3,9 @@
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash && \
     export NVM_DIR="/root/.nvm" && \
     source "$NVM_DIR/nvm.sh" && \ 
-    export SHELL="/bin/bash" && \
+    nvm use system && \
+    npm uninstall -g gulp && \
     npm config delete prefix && \
-    nvm debug && \
     nvm install 6.9.1 --delete-prefix && \
     npm install && \
     npm install -g gulp && \
