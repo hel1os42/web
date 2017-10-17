316057e73fef13f8bdccd8c8154946dcc47afb47
pasha <avdgian.pavel@gmail.com>
2017-10-17 08:56:26 +0300

    hardcode HOME

--- build_front.sh
+++ build_front.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash && \
-    export NVM_DIR="$HOME/.nvm" && \
+    export NVM_DIR="/root/.nvm" && \
     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && \ 
     npm config delete prefix && \
     nvm install 6.9.1 && \
