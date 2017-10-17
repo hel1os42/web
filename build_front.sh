d113eb5a867e27adfd733e900889f26cd049c7fb
pasha <avdgian.pavel@gmail.com>
2017-10-17 08:26:02 +0300

    sh

--- build_front.sh
+++ build_front.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash && \
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | sh && \
     export NVM_DIR="$HOME/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && \ 
     nvm use 6.9.1 && \
     npm install && \
