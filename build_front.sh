8ec24011d96f3f2b2f31e4e749858a7ea2beda0b
pasha <avdgian.pavel@gmail.com>
2017-10-17 11:11:40 +0300

    del node

--- build_front.sh
+++ build_front.sh
@@ -3,10 +3,9 @@
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash && \
     export NVM_DIR="/root/.nvm" && \
     export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  && \
     nvm use system && \
     npm config delete prefix && \
-    nvm debug && \
     nvm use  --delete-prefix 6.9.1 --silent && \
     nvm install 6.9.1 --delete-prefix && \
     npm install && \
