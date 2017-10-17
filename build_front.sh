585f708475d993b389ccc0c429558923312500bb
pasha <avdgian.pavel@gmail.com>
2017-10-17 08:45:04 +0300

    nvm install

--- build_front.sh
+++ build_front.sh
@@ -3,6 +3,7 @@
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash && \
     export NVM_DIR="$HOME/.nvm" && \
     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && \ 
+    nvm install 6.9.1 && \
     nvm use 6.9.1 && \
     npm install && \
     npm install -g gulp && \
