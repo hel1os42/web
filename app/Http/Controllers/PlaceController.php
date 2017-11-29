8abfd7bcc11fcfbc0b5c47afc3933037c333acb5
mobixon <mobixon@gmail.com>
2017-11-29 12:14:10 +0200

    #69 refactor

--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -6,7 +6,6 @@ use App\Helpers\Attributes;
 use App\Helpers\FormRequest;
 use App\Http\Requests\Place\CreateUpdateRequest;
 use App\Http\Requests\PlaceFilterRequest;
-use App\Models\NauModels\Offer;
 use App\Repositories\OfferRepository;
 use App\Repositories\PlaceRepository;
 use Illuminate\Auth\Access\AuthorizationException;
@@ -21,8 +20,6 @@ class PlaceController extends Controller
 {
     /**
      * @param PlaceFilterRequest $request
-     * @param PlaceRepository    $placesRepository
-     *
      * @param OfferRepository    $offerRepository
      *
      * @return Response
@@ -39,34 +36,13 @@ class PlaceController extends Controller
             ->getActiveByCategoriesAndPosition($request->category_ids,
                 $request->latitude, $request->longitude, $request->radius)
             ->select('acc_id')
-            ->groupBy('acc_id', 'lat', 'lng');
-
-        if (isset($request->latitude, $request->longitude)) {
-            $offers->orderByRaw(sprintf('(6371000 * 2 * 
-        ASIN(SQRT(POWER(SIN((lat - ABS(%1$s)) * 
-        PI()/180 / 2), 2) + COS(lat * PI()/180) * 
-        COS(ABS(%1$s) * PI()/180) * 
-        POWER(SIN((lng - %2$s) * 
-        PI()/180 / 2), 2))))',
-                \DB::connection()->getPdo()->quote($request->latitude),
-                \DB::connection()->getPdo()->quote($request->longitude)))
-                   ->groupBy('lat', 'lng');
-        }
-
-        $paginator = $offers->paginate();
+            ->groupBy('acc_id', 'lat', 'lng')
+            ->groupAndOrderByPosition($request->latitude, $request->longitude);
 
         $with = request()->get(config('repository.criteria.params.with', 'with'), null);
 
-        $array         = $paginator->toArray();
-        $array['data'] = $offers->get()->map(function (Offer $offer) use ($with) {
-            if (null !== $with) {
-                $with = explode(';', $with);
-
-                return $offer->getOwner()->place()->with($with)->first();
-            }
-
-            return $offer->getOwner()->place;
-        });
+        $array         = $offers->paginate()->toArray();
+        $array['data'] = $offers->getPlaces($with);
 
         return response()->render('place.index', $array);
     }
