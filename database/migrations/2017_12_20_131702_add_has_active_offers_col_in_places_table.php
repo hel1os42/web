0063b63096841081bc8d0e2c716ec01b2ee7c220
Aleksandr Stoilovskyi <mobixon@gmail.com>
2017-12-23 02:56:43 +0200

    fix in migration

--- database/migrations/2017_12_20_131702_add_has_active_offers_col_in_places_table.php
+++ database/migrations/2017_12_20_131702_add_has_active_offers_col_in_places_table.php
@@ -16,25 +16,6 @@ class AddHasActiveOffersColInPlacesTable extends Migration
         Schema::table('places', function (Blueprint $table) {
             $table->boolean('has_active_offers')->index()->default(0);
         });
-
-        $places = DB::table('places')->get();
-
-        foreach ($places as $place) {
-            $accountId = DB::connection('pgsql_nau')->table('account')->where('owner_id', $place->user_id)->get(['id']);
-
-            if (count($accountId)) {
-                $activeOffers = DB::connection('pgsql_nau')->table('offer')
-                                  ->where('acc_id', $accountId)
-                                  ->where('status', 'active')
-                                  ->get();
-                if (count($activeOffers) > 0) {
-                    DB::table('places')
-                      ->where('id', $place->id)
-                      ->update(['has_active_offers' => true]);
-                }
-            }
-        }
-
     }
 
     /**
