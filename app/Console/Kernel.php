fc6528f257cf435ddb7d63c455d827d388049f7b
Aleksandr Stoilovskyi <mobixon@gmail.com>
2017-12-24 16:08:33 +0200

    add coma

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        FixZeroReservedOffers::class
+        FixZeroReservedOffers::class,
     ];
 
     /**
