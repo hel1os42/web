c8cc063ca23adf52824af94d6d15a49a8eb01538
iLyK Necromancer <necromancer@toavalon.com>
2017-12-25 15:59:09 +0200

    added config value

--- config/nau.php
+++ config/nau.php
@@ -2,4 +2,5 @@
 return [
     'multiplier' => env('MULTIPLIER', 4),
     'reservation_multiplier' => env('NAU_RESERVATION_MULTIPLIER', 10),
+    'key.secret.investor' => env('INVESTOR_SECRET_KEY', 'deadbeef')
 ];
