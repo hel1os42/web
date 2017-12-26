3f5f71414be829e235146878cbbc49940f34ca38
Aleksandr Stoilovskyi <mobixon@gmail.com>
2017-12-26 17:23:45 +0200

    refactoring

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -10,14 +10,16 @@ class RedirectIfAuthenticated
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $guard
-     * @return mixed
+     * @param         $request
+     * @param Closure $next
+     * @param null    $guard
+     *
+     * @return \Illuminate\Http\RedirectResponse|mixed
+     * @throws \InvalidArgumentException
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check() && $request->route()->getName() !== 'register') {
+        if (Auth::guard($guard)->check()) {
             return redirect()->route('profile');
         }
 
