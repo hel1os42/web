cdb151afa8838563bd2c00535150bb887bb98aa3
iLyK Necromancer <necromancer@toavalon.com>
2017-12-28 18:11:13 +0200

    users registration

--- app/Http/Controllers/Service/NauController.php
+++ app/Http/Controllers/Service/NauController.php
@@ -19,6 +19,8 @@ use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
 /**
  * Class NauController
  * NS: App\Http\Controllers\Service
+ *
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  */
 class NauController extends Controller
 {
