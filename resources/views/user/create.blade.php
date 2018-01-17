a31999158e52e14b6ebdd2bfd653bf0c655f80bf
Aleksandr Stoilovskyi <mobixon@gmail.com>
2018-01-17 22:01:40 +0200

    #112 edit create user view

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -30,27 +30,8 @@
                                       placeholder="password_confirmation"></p>
                             <p><input style="line-height: 14px; font-size: 14px;" type="text" name="phone"
                                       value=""></p>
-                            <p>
-                                <input style="line-height: 14px; font-size: 14px;" type="radio"
-                                       onclick="checkUserRole(true)"
-                                       name="role_ids[]"
-                                       value="{{$roles::findByName('advertiser')->getId()}}" checked> Advertiser + user
-                            </p>
-                            <input hidden style="line-height: 14px; font-size: 14px;" type="checkbox"
-                                   id="role_ids_user"
-                                   name="role_ids[]"
-                                   value="{{$roles::findByName('user')->getId()}}" checked>
+                            @include('role-partials.selector', ['partialRoute' => 'user.create-role'])
 
-                            <p><input style="line-height: 14px; font-size: 14px;" type="radio"
-                                      onclick="checkUserRole(false)"
-                                      name="role_ids[]"
-                                      value="{{$roles::findByName('chief_advertiser')->getId()}}"> Chief advertiser</p>
-
-
-                            <input hidden style="line-height: 14px; font-size: 14px;" type="radio"
-                                   onclick="checkUserRole(false)"
-                                   name="parent_ids[]"
-                                   value="{{auth()->user()->getId()}}" checked>
                         </div>
                         <button type="submit">Create</button>
                     </div>
@@ -58,9 +39,4 @@
             </div>
         </div>
     </div>
-    <script type="application/javascript">
-        function checkUserRole( userFlag ) {
-            document.getElementById( 'role_ids_user' ).checked = userFlag;
-        }
-    </script>
 @stop
