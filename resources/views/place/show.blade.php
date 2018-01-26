f3570e797ef21434e6c512222577a9aadb9b6b0f
Aleksandr Stoilovskyi <mobixon@gmail.com>
2018-01-26 16:48:24 +0200

    edit place button

--- resources/views/place/show.blade.php
+++ resources/views/place/show.blade.php
@@ -3,12 +3,14 @@
 @section('title', 'NAU show Place')
 
 @section('content')
-    @if(!auth()->user()->isImpersonated())
-        <a href="{{route('profile.place.edit')}}">Edit place</a>
+    @if(!auth()->user()->isImpersonated()&& auth()->user()->isAdvertiser())
+        <a href="{{route('places.edit', [$id])}}">Edit place</a>
     @endif
     <div class="col-md-10 col-md-offset-1">
         <div class="card">
             <div class="content">
+                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                </a>
                 <div class="img-container text-center">
                     <img src="{{route('places.picture.show', [$id, 'cover'])}}"><br>
                 </div>
