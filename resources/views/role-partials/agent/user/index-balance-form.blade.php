e3c0a57a34b1ef7c79017aa895b297437e44999d
Aleksandr Stoilovskyi <mobixon@gmail.com>
2018-02-05 14:25:24 +0200

    #141 view edits

--- resources/views/role-partials/agent/user/index-balance-form.blade.php
+++ resources/views/role-partials/agent/user/index-balance-form.blade.php
@@ -1,1 +1,1 @@
-<input hidden type="text" name="source" id="source" value="{{auth()->user()->getAccountForNau()->getAddress()}}">
+<input type="hidden" name="source" id="source" value="{{auth()->user()->getAccountForNau()->getAddress()}}">
