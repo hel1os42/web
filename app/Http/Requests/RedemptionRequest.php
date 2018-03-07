3fb38ccd9185ab820c9f1962ec5dc09a689eacd3
Aleksandr Stoilovskyi <mobixon@gmail.com>
2018-03-07 12:28:08 +0200

    #208 fix otp stub should work only in needed environments

--- app/Http/Requests/RedemptionRequest.php
+++ app/Http/Requests/RedemptionRequest.php
@@ -37,7 +37,7 @@ class RedemptionRequest extends FormRequest
     public function rules()
     {
         return [
-            'code' => 'required|can_redeem'
+            'code' => 'required|can_redeem|min:3'
         ];
     }
 } 
