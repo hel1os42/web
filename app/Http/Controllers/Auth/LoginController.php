ad79140b35b21d0326351abb35388ed8db0bd147
lex <lex.codeworld@gmail.com>
2018-03-15 11:46:36 +0200

    remark

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -73,24 +73,15 @@ class LoginController extends AuthController
      */
     public function logout()
     {
-        if ($this->auth->guard()->user() instanceof Operator) {
-
-            $this->auth->guard()->logout();
-
-            return \request()->wantsJson()
-                ? \response()->render('', '', Response::HTTP_NO_CONTENT)
-                : \redirect()->route('loginFormOperator');
-        }
-
-        if (false === $this->jwtAuth->getToken()) {
-                $this->user()->leaveImpersonation();
+        if (false === $this->jwtAuth->getToken() && !$this->auth->guard()->user() instanceof Operator) {
+            $this->user()->leaveImpersonation();
         }
 
         $this->auth->guard()->logout();
 
         return \request()->wantsJson()
             ? \response()->render('', '', Response::HTTP_NO_CONTENT)
-            : \redirect()->route('login');
+            : \redirect()->route($this->auth->guard()->user() instanceof Operator ? 'loginFormOperator' : 'login');
     }
 
     /**
