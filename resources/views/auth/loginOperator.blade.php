0bb44a207d56627498dcb4e43df49c353f3bed4c
a.hutsul <a.hutsul@omnisynapse.ca>
2018-03-16 18:22:56 +0200

    added validation rules

--- resources/views/auth/loginOperator.blade.php
+++ resources/views/auth/loginOperator.blade.php
@@ -40,21 +40,6 @@
     </div>
 </div>
 
-@push('styles')
-    <style>
-        .login-page .card { padding-top: 0; padding-bottom: 6px; }
-        .tabs-titles { position: relative; z-index: 2; margin: 0; padding: 0; list-style: none; }
-        .tabs-titles:after { content: ""; display: block; clear: both; }
-        .tabs-titles li { float: left; }
-        .tabs-titles li a { position: relative; display: block; padding: 6px 15px; color: #364150; border: 1px solid #eee; border-bottom: none; border-radius: 6px 6px 0 0;}
-        .tabs-titles li.active a { color: #fff; background: #f08301 linear-gradient(to right, #f08301, #f0a810); }
-        .tabs-titles li.active a:after { content: ''; position: absolute; left: 0; top: 100%; right: 0; height: 1px; background: #f08301 linear-gradient(to right, #f08301, #f0a810); }
-        .tabs-titles li:not(:first-child) { margin-left: -1px; }
-        .tabs .tab { display: none; padding: 12px 15px; border: 1px solid #eee; border-radius: 0 0 6px 6px; }
-        .tabs .tab.active { display: block; }
-        #formOperator { margin-bottom: 6px; }
-    </style>
-@endpush
 
 @push('scripts')
 <script src="{{ asset('js/jquery.min.js') }}"></script>
