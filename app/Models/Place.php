c3d3629dea125274c0cf678ed99e77e7251a1eb7
Aleksandr Stoilovskyi <mobixon@gmail.com>
2018-04-04 13:30:09 +0300

    #227 timezone offset in minutes

--- app/Models/Place.php
+++ app/Models/Place.php
@@ -212,7 +212,7 @@ class Place extends Model
 
         $timezoneOffsetInSec = $timezone->getOffset($currentDate);
 
-        return sprintf("%+03d00", $timezoneOffsetInSec / 3600);
+        return sprintf("%+03d%02d", $timezoneOffsetInSec / 3600, ($timezoneOffsetInSec % 3600)/60);
     }
 
     public function getActiveOffersCountAttribute(): int
