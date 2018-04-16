190f5ca7fc56bcbf7bcf4ef4f0fb64b951b5918e
a.hutsul <a.hutsul@omnisynapse.ca>
2018-04-16 18:20:26 +0300

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,36 +4,51 @@ namespace App\Http\Controllers;
 
 use App\Models\Role;
 use App\Models\User;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Support\Collection;
 use Illuminate\Http\Response;
+use App\Repositories\RoleRepository;
+use App\Repositories\OfferRepository;
+
 
 class HomeController extends Controller
 {
     /**
+     * @param RoleRepository $roleRepository
+     * @param OfferRepository $offerRepository
+     * @param Authenticatable $authUser
+     *
      * @return Response
      */
-    public function index()
+    public function index(
+        RoleRepository $roleRepository,
+        OfferRepository $offerRepository,
+        Authenticatable $authUser
+    ): Response
     {
-        if (auth()->user() instanceof \App\Models\Operator) {
+        if ($authUser instanceof \App\Models\Operator) {
             return \response()->render('operator', []);
         }
 
         $table = null;
 
         if ($this->user()->isAdmin()) {
-            $table = $this->getAdminStatistic();
+            $table = $this->getAdminStatistic($roleRepository);
         } elseif ($this->user()->isAgent()) {
-            $table = $this->getAgentStatistic();
+            $table = $this->getAgentStatistic($offerRepository);
         }
 
         return \response()->render('home', ['table' => $table]);
     }
 
     /**
+     * @param RoleRepository $roleRepository
+     *
      * @return Collection
      */
-    protected function getAdminStatistic() {
-        $users = app('roleRepository')->scopeQuery(function (Role $query) {
+    protected function getAdminStatistic(RoleRepository $roleRepository): Collection
+    {
+        $users = $roleRepository->scopeQuery(function (Role $query) {
             return $query->join('users_roles', 'users_roles.role_id', 'roles.id');
         })
             ->get(['name'])
@@ -52,13 +67,16 @@ class HomeController extends Controller
     }
 
     /**
+     * @param OfferRepository $offerRepository
+     *
      * @return array
      */
-    protected function getAgentStatistic() {
+    protected function getAgentStatistic(OfferRepository $offerRepository): array
+    {
         $users = $this->user()->children;
 
-        $offersByUser = $users->map(function (User $user) {
-            return app('offerRepository')
+        $offersByUser = $users->map(function (User $user) use ($offerRepository) {
+            return $offerRepository
                 ->scopeAccount($user->getAccountForNau())
                 ->withoutGlobalScopes()
                 ->withCount(['redemptions'])
