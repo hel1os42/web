7c69e9a5ac42bf2abb089351802da3b74c82d60b
a.hutsul <a.hutsul@omnisynapse.ca>
2018-05-11 16:10:30 +0300

    #225 fix

--- resources/views/role-partials/agent/user/show-edit.blade.php
+++ resources/views/role-partials/agent/user/show-edit.blade.php
@@ -1,33 +1,33 @@
 @can('user.update.children', [$editableUserModel, array_column($children, 'id')])
 
-<div class="row">
-    <div class="col-sm-3">
-        <p><strong>Children</strong></p>
+    <div class="row">
+        <div class="col-sm-3">
+            <p><strong>Children</strong></p>
+        </div>
+
+        <div class="col-sm-9">
+            @if(isset($children))
+                <div class="children-wrap" style="padding-bottom: 16px;">
+                    <input type="hidden" name="child_ids[]">
+
+                    @foreach($children as $child)
+                        <p>
+                            <input type="hidden"
+                                   class="added-children"
+                                   name="child_ids[]"
+                                   value="{{ $child['id'] }}"
+                            >
+                            <strong>{{ $child['name'] }} ({{ $child['email'] }})</strong>
+                            <button type="button" class="close rm_child">×</button>
+                        </p>
+                    @endforeach
+                </div>
+            @endif
+            <button id="add_children" class="btn" type="button" data-toggle="modal" data-target="#add_children_list">
+                Add children
+            </button>
+
+        </div>
     </div>
-
-    <div class="col-sm-9">
-        @if(isset($children))
-            <div class="children-wrap" style="padding-bottom: 16px;">
-                <input type="hidden" name="child_ids[]">
-
-                @foreach($children as $child)
-                    <p>
-                        <input type="hidden"
-                               class="added-children"
-                               name="child_ids[]"
-                               value="{{ $child['id'] }}"
-                        >
-                        <strong>{{ $child['name'] }} ({{ $child['email'] }})</strong>
-                        <button type="button" class="close rm_child">×</button>
-                    </p>
-                @endforeach
-            </div>
-        @endif
-        <button id="add_children" class="btn" type="button" data-toggle="modal" data-target="#add_children_list">
-            Add children
-        </button>
-
-    </div>
-</div>
 
 @endcan
