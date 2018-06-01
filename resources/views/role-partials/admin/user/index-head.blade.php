ed9c472701f3800b782e18039ca3c1d14da6b78c
a.hutsul <a.hutsul@omnisynapse.ca>
2018-06-01 15:28:41 +0300

    fixed admin search markup

--- resources/views/role-partials/admin/user/index-head.blade.php
+++ resources/views/role-partials/admin/user/index-head.blade.php
@@ -1,13 +1,17 @@
 <h1>Users list</h1>
-<a href="{{ route('users.create') }}" style="float:right" class="btn">+ Add new user</a>
 <div id="admin-users-search">
-    <div>
-        <p>By user:&nbsp;&nbsp;
-            <input type="text" name="name" id="name" value="" placeholder="name">
-            <input type="text" name="email" id="email" value="" placeholder="email">
-            <input type="text" name="phone" id="phone" value="" placeholder="phone">
+    <div class="m-b-20">
+        <p>
+            <label for="search_fields">
+                <b>Search by user (name, email, phone) or place (name, description):</b>
+            </label>
+        </p>
+
+        <input type="text" name="search_fields" id="search_fields" value="" style="width: 400px;" />
+
+        <label>
             <select name="role" id="role"
-                    style="padding: 1px 0px; width: 199px;  border: 1px solid darkgrey; height: 28px;">
+                    style="padding: 1px 0px; width: 199px;  border: 1px solid darkgrey; height: 30px;">
                 <option value="" selected>All roles</option>
                 <option value="admin">Admin</option>
                 <option value="agent">Agent</option>
@@ -15,19 +19,16 @@
                 <option value="advertiser">Advertiser</option>
                 <option value="user">User</option>
             </select>
-        </p>
+        </label>
 
+        <form method="get" action="{{ route('users.index') }}" id="search-form" style="display: inline-block;">
+            <input type="hidden" name="search" id="search-field" value="">
+            <input type="hidden" name="searchJoin" value="and">
+            <button type="submit" class="btn m-l-10">Search</button>
+        </form>
     </div>
-    <div>
-        <p>By place:&nbsp;
-            <input type="text" name="place.name" id="place-name" value="" placeholder="name">
-            <input type="text" name="place.description" id="place-description" value="" placeholder="description">
-        </p>
-    </div>
+</div>
 
-    <form method="get" action="{{ route('users.index') }}" id="search-form" style="display: inline-block;">
-        <input type="hidden" name="search" id="search-field" value="">
-        <input type="hidden" name="searchJoin" value="and">
-        <button type="submit" class="btn">Search</button>
-    </form>
+<div class="clearfix">
+    <a href="{{ route('users.create') }}" style="float:right" class="btn">+ Add new user</a>
 </div>
