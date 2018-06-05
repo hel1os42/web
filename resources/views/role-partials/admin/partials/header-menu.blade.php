b7f5774fcd7aba3c41b42fc74f4cbe34e9203ad6
lex <lex.codeworld@gmail.com>
2018-06-05 11:06:24 +0300

    #271 revert cahges

--- resources/views/role-partials/admin/partials/header-menu.blade.php
+++ resources/views/role-partials/admin/partials/header-menu.blade.php
@@ -6,11 +6,8 @@
     <a href="{{ route('categories.index') }}">Categories & retail types</a>
 </li>
 <li>
-    <a href="{{ route('tags.index') }}">Tags</a>
-</li>
-
-
-
-<li>
     <a href="{{ route('settings.index') }}">Settings</a>
+</li>
+<li>
+    <a href="{{ route('tags.index') }}">Tags</a>
 </li>
