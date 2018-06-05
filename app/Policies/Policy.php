d71b72881170317e962e83231d93c0928aefd90c
lex <lex.codeworld@gmail.com>
2018-06-05 11:33:38 +0300

    #271 phpmd

--- app/Policies/Policy.php
+++ app/Policies/Policy.php
@@ -4,6 +4,13 @@ namespace App\Policies;
 
 use Illuminate\Auth\Access\HandlesAuthorization;
 
+/**
+ * Class Policy
+ *
+ * @package App\Policies
+ *
+ * @SuppressWarnings(PHPMD.NumberOfChildren)
+ */
 class Policy
 {
     use HandlesAuthorization;
