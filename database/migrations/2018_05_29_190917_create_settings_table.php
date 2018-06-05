0a883d70745b6cee0d358ca0978c8168871cf997
lex <lex.codeworld@gmail.com>
2018-06-05 15:22:04 +0300

    #271 fx migration field length

--- database/migrations/2018_05_29_190917_create_settings_table.php
+++ database/migrations/2018_05_29_190917_create_settings_table.php
@@ -30,7 +30,7 @@ class CreateSettingsTable extends Migration
         Schema::create($this->tablename, function(Blueprint $table)
         {
             $table->increments('id');
-            $table->string($this->keyColumn)->index();
+            $table->string($this->keyColumn, 191)->index();
             $table->text($this->valueColumn);
         });
 
