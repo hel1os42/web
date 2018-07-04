5aadee6b21c230e8074ceb9e9e94e086f6d3e60d
Oleg <oleg@toavalon.com>
2018-07-04 15:37:32 +0000

    Revert "Merge branch 'create-offer-image-validation' into 'master'"
    This reverts merge request !854

--- resources/lang/en/offers.php
+++ resources/lang/en/offers.php
@@ -1,28 +1,18 @@
 <?php
 
 return [
-    'points_for_redemption'             => 'Points for redemption',
-    'fields'                            => [
-        'label'                         => 'Label',
-        'is_featured'                   => 'Featured',
-        'referral_points_price'         => 'Friend points price',
-        'redemption_points_price'       => 'Redemption points price',
+    'points_for_redemption' => 'Points for redemption',
+    'fields'                => [
+        'label'                   => 'Label',
+        'is_featured'             => 'Featured',
+        'referral_points_price'   => 'Friend points price',
+        'redemption_points_price' => 'Redemption points price',
     ],
-    'text'                              => [
-        'featured_options'              => 'Featured options',
-        'account_tokens'                => 'You have tokens on your account',
+    'text'                  => [
+        'featured_options' => 'Featured options',
+        'account_tokens'   => 'You have tokens on your account',
     ],
-    'buttons'                           => [
-        'create_offer'                  => 'Create Offer',
+    'buttons'               => [
+        'create_offer' => 'Create Offer',
     ],
-    'picture'                           => [
-        'title'                         => 'Offer image',
-        'hint_invalid'                  => 'To create offer you have to upload image',
-        'requirements'                  => 'Image requirements',
-        'requirements_format'           => 'format',
-        'requirements_max_size'         => 'maximum size',
-        'recomended'                    => 'Recommended image',
-        'recomended_cover'              => 'fine cover for mobile app page',
-        'recomended_size'               => 'up to',
-    ]
 ];
