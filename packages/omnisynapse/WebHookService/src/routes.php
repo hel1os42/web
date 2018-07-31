c2ffdc8e07931338c6e4ddd401cfc05aa5a269f0
Taras Omelianchuk <taras@toavalon.com>
2018-07-31 10:12:08 +0300

    #162 Fixed build with integrational tests

--- packages/omnisynapse/WebHookService/src/routes.php
+++ packages/omnisynapse/WebHookService/src/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use \Illuminate\Support\Facades\Route;
+
 Route::group(['middleware' => 'auth:jwt'], function () {
     Route::resource('/webhooks', \OmniSynapse\WebHookService\Http\Controllers\WebHookController::class, ['except' => [
         'create', 'edit'
