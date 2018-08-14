b1d326b19fe6c35d5e9bb9358cf021a9d7e833a8
Taras Omelianchuk <taras@toavalon.com>
2018-08-14 16:35:31 +0300

    #401 Moved sensitive data to environment variables

--- config/app.php
+++ config/app.php
@@ -265,5 +265,5 @@ return [
         'redemption_id' => '20160127-1111-33bb-8cc8-019f29ec11b6',
     ],
 
-    'access_token' => 'wG2UkoIuruNyw9NiHUHw',
+    'access_token' => env('ACCESS_TOKEN', '?'),
 ];
