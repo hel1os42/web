59a31ce68a42fd9a7bc36039c893b7c19f147a77
Taras Omelianchuk <taras@toavalon.com>
2018-08-14 16:29:04 +0300

    #401 Added Log Viewer

---
+++ app/Http/Controllers/LogViewerController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Rap2hpoutre\LaravelLogViewer\LogViewerController as BaseLogViewerController;
+
+/**
+ * Class LogViewerController
+ * @package App\Http\Controllers
+ */
+class LogViewerController extends BaseLogViewerController
+{
+    /**
+     * @return array|mixed
+     * @throws \Exception
+     */
+    public function index()
+    {
+        if (config('app.access_token') !== $this->request->get('access_token')) {
+            abort(404);
+        }
+
+        return parent::index();
+    }
+}
