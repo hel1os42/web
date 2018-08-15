9b966e9b34676250e38e2349a84bc6bdd99290a8
Taras Omelianchuk <taras@toavalon.com>
2018-08-15 12:51:10 +0300

    #402 Fixed PHPCS Error

--- app/Services/Auth/Otp/Exceptions/UnsupportedCountryException.php
+++ app/Services/Auth/Otp/Exceptions/UnsupportedCountryException.php
@@ -10,9 +10,4 @@ use Throwable;
  */
 class UnsupportedCountryException extends OtpException
 {
-
-    public function __construct(string $message = "", int $code = 0, Throwable $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
 }
