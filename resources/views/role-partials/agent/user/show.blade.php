f0882441809be9cb8e851cdf3cc20f931ae8231c
lex <lex.codeworld@gmail.com>
2018-08-23 20:03:23 +0300

    305 - removed activity control element

--- resources/views/role-partials/agent/user/show.blade.php
+++ resources/views/role-partials/agent/user/show.blade.php
@@ -42,15 +42,6 @@
             <span class="span-approved">{{ __('words.yes') }}</span>
             <span class="span-disapproved">{{ __('words.no') }}</span>
             <span class="span-wait">...</span>
-
-            <form action="{{ route('users.update', $id) }}" method="POST">
-                {{ csrf_field() }}
-                {{ method_field('PATCH') }}
-                <input type="hidden" name="approved" value="{{ $approved ? '0' : '1' }}">
-                <button type="submit" class="btn btn-xs b-approved">{{ __('buttons.disapprove') }}</button>
-                <button type="submit" class="btn btn-xs b-disapproved">{{ __('buttons.approve') }}</button>
-                <span class="waiting-response"><img src="{{ asset('img/loading.gif') }}" alt="wait..."></span>
-            </form>
         </div>
     </div>
 </div>
